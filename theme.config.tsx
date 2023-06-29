4137b4f1a6c1e6fd99d7b0bf85ccb3b98d195be4
Burhan Uddin <burhan.bd@gmail.com>
2023-06-30 01:24:20 +0600

    first commit

---
+++ theme.config.tsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import { DocsThemeConfig } from 'nextra-theme-docs'
+
+const config: DocsThemeConfig = {
+  logo: <span>Revealing the Unspoken: Dr. Ruhul Amin Shajib and the Untold Story of Pipilika Search Engine</span>,
+  // project: {
+  //   link: 'https://github.com/shuding/nextra-docs-template',
+  // },
+  // chat: {
+  //   link: 'https://discord.com',
+  // },
+  // docsRepositoryBase: 'https://github.com/shuding/nextra-docs-template',
+  footer: {
+    text: 'Revealing the Unspoken: Dr. Ruhul Amin Shajib and the Untold Story of Pipilika Search Engine',
+  },
+  feedback:{ content: null }, 
+  editLink: { text: null }
+}
+
+export default config
