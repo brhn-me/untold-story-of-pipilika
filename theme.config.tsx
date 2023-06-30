7d941c0b246aa1777c38baa59dadcfbadbe36f13
Burhan Uddin <burhan.bd@gmail.com>
2023-06-30 14:05:21 +0600

    added original logo

--- theme.config.tsx
+++ theme.config.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { DocsThemeConfig } from 'nextra-theme-docs'
 
 const config: DocsThemeConfig = {
-  logo: <span>Revealing the Unspoken: Dr. Ruhul Amin Shajib and the Untold Story of Pipilika Search Engine</span>,
+  logo: <span><img src='/static/pipilika/logo-small.png' /></span>,
   // project: {
   //   link: 'https://github.com/shuding/nextra-docs-template',
   // },
@@ -10,8 +10,16 @@ const config: DocsThemeConfig = {
   //   link: 'https://discord.com',
   // },
   // docsRepositoryBase: 'https://github.com/shuding/nextra-docs-template',
+  head: (
+    <>
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      <meta property="og:title" content="Pipilika Search Engine - The Untold Story" />
+      <meta property="og:description" content="Revealing the unspoken dirty manipulation of Dr. Ruhul Amin during early days of the Pipilika Search Engine" />
+      <link rel="icon" type="image/x-icon" href="/static/pipilika/favicon.ico"></link>
+    </>
+  ),
   footer: {
-    text: 'Revealing the Unspoken: Dr. Ruhul Amin Shajib and the Untold Story of Pipilika Search Engine',
+    text: 'Pipilika Search Engine - The Untold Story',
   },
   feedback:{ content: null }, 
   editLink: { text: null }
